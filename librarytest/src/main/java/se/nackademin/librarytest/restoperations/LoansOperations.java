ad55dc9095b805f1d3d47308cbcdfa16f3d2a7eb
Jesper Ragnarsson <ragnarsson.jesper@gmail.com>
2016-06-02 10:44:53 +0200

    Added remaining loan tests (not entirely functional as I have not figured out the put command just yet)

--- librarytest/src/main/java/se/nackademin/librarytest/restoperations/LoansOperations.java
+++ librarytest/src/main/java/se/nackademin/librarytest/restoperations/LoansOperations.java
@@ -54,4 +54,71 @@ public class LoansOperations {
         return getResponse;
     }
     
+    public Response getAllLoans(){
+        String resourceName = "loans";
+        Response getResponse = given().accept(ContentType.JSON).get(BASE_URL+resourceName);
+        return getResponse;
+    }
+    
+    public Response updateLoanById(int id){
+        String resourceName = "loans";
+        String dateBorrowed = randomizers.generateRandomDate();
+        String dateDue = randomizers.generateRandomDate();
+        String putBodyTemplate = "{\n"
+                + "    \"loans\": {\n"
+                + "        \"loan\": {\n"
+                + "            \"id\": %s,\n"
+                + "            \"book\": {\n"
+                + "                \"id\": 1,\n"
+                + "                \"author\": {\n"
+                + "                    \"id\": 1,\n"
+                + "                    \"bio\": \"Margaret Eleanor Atwood, CC OOnt FRSC (born November 18, 1939) is a Canadian poet, novelist, literary critic, essayist, and environmental activist. She is a winner of the Arthur C. Clarke Award and Prince of Asturias Award for Literature, has been shortlisted for the Booker Prize five times, winning once, and has been a finalist for the Governor General's Award several times, winning twice. In 2001, she was inducted into Canada's Walk of Fame.\",\n"
+                + "                    \"country\": \"Canada\",\n"
+                + "                    \"firstName\": \"Margaret\",\n"
+                + "                    \"lastName\": \"Atwood\"\n"
+                + "                },\n"
+                + "                \"description\": \"The novel focuses on a post-apocalyptic character named Snowman, living near a group of primitive human-like creatures whom he calls Crakers.\",\n"
+                + "                \"isbn\": \"0-7710-0868-6\",\n"
+                + "                \"nbrPages\": 411,\n"
+                + "                \"publicationDate\": \"2003-04-01\",\n"
+                + "                \"title\": \"Oryx and Crake\",\n"
+                + "                \"totalNbrCopies\": 1\n"
+                + "            },\n"
+                + "            \"dateBorrowed\": \"%s\",\n"
+                + "            \"dateDue\": \"%s\",\n"
+                + "            \"user\": {\n"
+                + "                \"id\": 13,\n"
+                + "                \"displayName\": \"lennart\",\n"
+                + "                \"email\": \"lennart@example.com\",\n"
+                + "                \"firstName\": \"Lennart\",\n"
+                + "                \"lastName\": \"Moraeus\",\n"
+                + "                \"password\": \"sha1:64000:18:qwd4zyLXk7On2Ra3QQf9YcfuR7nyM3if:IrbdhXxgxjeOZZzBqvxuM6B7\",\n"
+                + "                \"phone\": \"+46000333444\",\n"
+                + "                \"role\": \"LOANER\"\n"
+                + "            }\n"
+                + "        }\n"
+                + "    }\n"
+                + "}";
+        String putBody = String.format(putBodyTemplate, id, dateBorrowed, dateDue);
+        Response putResponse = given().contentType(ContentType.JSON).body(putBody).put(BASE_URL+resourceName);
+        return putResponse;
+    }
+    
+    public Response getLoansOfUser(int id){
+        String resourceName = "loans/ofuser/"+id;
+        Response getResponse = given().accept(ContentType.JSON).get(BASE_URL+resourceName);
+        return getResponse;
+    }
+    
+    public Response getLoansOfBook(int id){
+        String resourceName = "loans/ofbook/"+id;
+        Response getResponse = given().accept(ContentType.JSON).get(BASE_URL+resourceName);
+        return getResponse;
+    }
+    
+    public Response getLoanOfBookByUser(int bookId, int userId){
+        String resourceName = "loans/ofuser/"+userId+"/ofbook/"+bookId;
+        Response getResponse = given().accept(ContentType.JSON).get(BASE_URL+resourceName);
+        return getResponse;
+    }
 }
--- librarytest/src/test/java/se/nackademin/librarytest/rest/LoansTest.java
+++ librarytest/src/test/java/se/nackademin/librarytest/rest/LoansTest.java
@@ -8,7 +8,7 @@ import se.nackademin.librarytest.restoperations.LoansOperations;
 
 public class LoansTest {
     
-    // /loans endpoint coverage (1/3)
+    // /loans endpoint coverage (3/3)
 
     @Test
     public void testCreateLoan(){
@@ -21,7 +21,27 @@ public class LoansTest {
         assertEquals("Status code should be 204", 204, deleteResponse.getStatusCode());
     }
     
-    // /loans/{id} endpoint coverage (1/2)
+    @Test
+    public void testUpdateLoan(){
+        Response postResponse = new LoansOperations().createNewLoan(1, 13);
+        assertEquals("Status code should be 201", 201, postResponse.getStatusCode());
+        
+        int latestLoanId = new LoansOperations().getLatestLoanId();
+        
+        Response putResponse = new LoansOperations().updateLoanById(latestLoanId);
+        assertEquals("Status code should be 200", 200, putResponse.getStatusCode());
+        
+        Response deleteResponse = new LoansOperations().deleteLoanById(latestLoanId);
+        assertEquals("Status code should be 204", 204, deleteResponse.getStatusCode());
+    }
+    
+    @Test
+    public void testGetAllLoans(){
+        Response getResponse = new LoansOperations().getAllLoans();
+        assertEquals("Status code should be 200", 200, getResponse.getStatusCode());
+    }
+    
+    // /loans/{id} endpoint coverage (2/2)
     
     @Test
     public void testGetLoan(){
@@ -35,5 +55,52 @@ public class LoansTest {
         
         Response deleteResponse = new LoansOperations().deleteLoanById(latestLoanId);
         assertEquals("Status code should be 204", 204, deleteResponse.getStatusCode());
+    }
+    
+    @Test
+    public void testDeleteLoan(){
+        Response postResponse = new LoansOperations().createNewLoan(1, 13);
+        assertEquals("Status code should be 201", 201, postResponse.getStatusCode());
+        
+        int latestLoanId = new LoansOperations().getLatestLoanId();
+        
+        Response deleteResponse = new LoansOperations().deleteLoanById(latestLoanId);
+        assertEquals("Status code should be 204", 204, deleteResponse.getStatusCode());
+        
+        Response response = new LoansOperations().getLoanById(latestLoanId);
+        assertEquals("Status code should be 404", 404, response.getStatusCode());
+    }
+    
+    // /loans/ofuser/{user_id} endpoint coverage (1/1)
+    
+    @Test
+    public void testGetLoansOfUser(){
+        Response postResponse = new LoansOperations().createNewLoan(1, 13);
+        assertEquals("Status code should be 201", 201, postResponse.getStatusCode());
+        
+        Response getResponse = new LoansOperations().getLoansOfUser(13);
+        assertEquals("Status code should be 200", 200, getResponse.getStatusCode());
+    }
+    
+    // /loans/ofbook/{book_id} endpoint coverage (1/1)
+    
+    @Test
+    public void testGetAllLoansOfBook(){
+        Response postResponse = new LoansOperations().createNewLoan(1, 13);
+        assertEquals("Status code should be 201", 201, postResponse.getStatusCode());
+        
+        Response getResponse = new LoansOperations().getLoansOfBook(1);
+        assertEquals("Status code should be 200", 200, getResponse.getStatusCode());
+    }
+    
+    // /loans/ofuser/{user_id}/ofbook/{book_id} endpoint coverage (1/1)
+    
+    @Test
+    public void testGetLoanOfBookByUser(){
+        Response postResponse = new LoansOperations().createNewLoan(1, 13);
+        assertEquals("Status code should be 201", 201, postResponse.getStatusCode());
+        
+        Response getResponse = new LoansOperations().getLoanOfBookByUser(1, 13);
+        assertEquals("Status code should be 200", 200, getResponse.getStatusCode());
     }
 }
